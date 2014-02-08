e29b469bd47ec2dc9181c481ddeea9b602aa486d
jnphilipp <jnphilipp@gmail.com>
2014-02-08 18:19:05 +0100

    Seperate method for notify.

--- Main.java
+++ Main.java
@@ -1,22 +1,13 @@
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.util.Date.*;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
-import javax.net.ssl.HttpsURLConnection;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
 
 
 public class Main {
@@ -24,6 +15,7 @@ public class Main {
 	/**
 	 * @param args
 	 */
+	public static final String ICON_PATH = "/tmp/BitCoinIconForBitCoinTicker.png";
 	private final String USER_AGENT = "Mozilla/6.0";
 	public int currentprice;
 
@@ -38,7 +30,7 @@ public class Main {
             URL url =new URL("http://i.imgur.com/6bNY1sH.png");
             // read the url
            image = ImageIO.read(url);
-           ImageIO.write(image, "png",new File("/tmp/BitCoinIconForBitCoinTicker.png"));
+           ImageIO.write(image, "png",new File(Main.ICON_PATH));
         }catch(IOException e){
             e.printStackTrace();
        System.out.println("Something went wrong, you likely the file is gone :(");
@@ -89,10 +81,11 @@ public class Main {
 			while ((inputLine = in.readLine()) != null) {
 				// response.append(inputLine);
 				i++;
-				if (i == 5) {
-					price = inputLine;
-				}
-				
+				Pattern p = Pattern.compile("last\\\":\\s(.+),$");
+				Matcher m = p.matcher(inputLine);
+				if ( m.find() )
+					price = m.group(1);
+
 				if (i == 2) {
 					String avg = inputLine;
 					System.out.println(avg.replace("\"24h_avg\":", ""));
@@ -102,49 +95,27 @@ public class Main {
 					int ave = Integer.parseInt(avgfixed3.replace(".",""));
 				    realaverage = ave / 100;
 				}
-				
-
 			}
+			in.close();
 
 			
 
 	System.out.println(realaverage);
-			System.out.println(price);
-			String id = price.replace("\"last\":", "") + "$";
-			
-			String q = id.replace(",$", "");
-			String e = q.replace("   ","");
-			String ui = e.replace(".", "");
-			int y = Integer.parseInt(ui);
-			if(y-realaverage > 0){
-			
-			Process p = Runtime
-					.getRuntime()
-					.exec(new String[] { "/usr/bin/notify-send", "-i", "/tmp/BitCoinIconForBitCoinTicker.png",
-							"The Price Of Bitcoin Is Up from the daily average  at ", id.replace(",", "") });
-			int currentprice = y;
-			}
-			if(y-realaverage < 0){
-				Process p = Runtime
-						.getRuntime()
-						.exec(new String[] { "/usr/bin/notify-send", "-i", "/tmp/BitCoinIconForBitCoinTicker.png",
-								"The Price Of Bitcoin Is Down from the daily average at ", id.replace(",", "") });
-			}
-			if(y-realaverage == 0){
-				Process p = Runtime
-						.getRuntime()
-						.exec(new String[] { "/usr/bin/notify-send", "-i", "/tmp/BitCoinIconForBitCoinTicker.png",
-								"The Price Of Bitcoin Is at the daily average at ", id.replace(",", "") });
-			}
-			in.close();
-		
+
+			float y = Float.parseFloat(price);
+			if(y-realaverage > 0)
+				this.sendNotify("The Price Of Bitcoin Is Up from the daily average  at " + price + "$");
+			if(y-realaverage < 0)
+				this.sendNotify("The Price Of Bitcoin Is Down from the daily average at " + price + "$");
+			if(y-realaverage == 0)
+				this.sendNotify("The Price Of Bitcoin Is at the daily average at " + price + "$");
+
 			Thread.sleep(timetosleep*60000);
-		
 		}
 	}
 
-
-	{
+	private void sendNotify(String text) throws IOException {
+		Runtime.getRuntime().exec(new String[] {"/usr/bin/notify-send", "-i", Main.ICON_PATH, text});
 	}
 
 }
